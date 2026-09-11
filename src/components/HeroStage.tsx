3c26da7d9ef69c4b9aa8e9824b18a7f87b261246
jakuninoleg <oleg.kemz@gmail.com>
2026-09-11 20:08:22 +0300

    Replace mascot with clean-face render, gray-bg cutout only.

--- src/components/HeroStage.tsx
+++ src/components/HeroStage.tsx
@@ -8,7 +8,7 @@ export function HeroStage() {
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         className="hero-mascot"
-        src="/mascot/mascot-base.webp?v=gen-clean1"
+        src="/mascot/mascot-base.webp?v=face-clean3"
         alt=""
         width={900}
         height={900}
