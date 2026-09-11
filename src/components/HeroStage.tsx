2fde708c40b4ea3730e10f6c8cc26f72de5ce495
jakuninoleg <oleg.kemz@gmail.com>
2026-09-11 20:15:20 +0300

    Bust mascot cache for final clean cutout.

--- src/components/HeroStage.tsx
+++ src/components/HeroStage.tsx
@@ -8,7 +8,7 @@ export function HeroStage() {
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         className="hero-mascot"
-        src="/mascot/mascot-base.webp?v=raw-gen1"
+        src="/mascot/mascot-base.webp?v=final-ok1"
         alt=""
         width={900}
         height={900}
