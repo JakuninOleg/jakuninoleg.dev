01e56469accb57dad1a60c45e3eee68011b896bf
jakuninoleg <oleg.kemz@gmail.com>
2026-09-11 19:54:36 +0300

    Restore early clean mascot without white contour fringe.

--- src/components/HeroStage.tsx
+++ src/components/HeroStage.tsx
@@ -8,7 +8,7 @@ export function HeroStage() {
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         className="hero-mascot"
-        src="/mascot/mascot-base.webp?v=static1"
+        src="/mascot/mascot-base.webp?v=early-clean"
         alt=""
         width={900}
         height={900}
