da6329aef0a39e7258298aba25512c6c6fe4aeef
jakuninoleg <oleg.kemz@gmail.com>
2026-09-11 20:01:46 +0300

    Ship generated clean mascot cutout without pants holes.

--- src/components/HeroStage.tsx
+++ src/components/HeroStage.tsx
@@ -8,7 +8,7 @@ export function HeroStage() {
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         className="hero-mascot"
-        src="/mascot/mascot-base.webp?v=early-clean"
+        src="/mascot/mascot-base.webp?v=gen-clean1"
         alt=""
         width={900}
         height={900}
